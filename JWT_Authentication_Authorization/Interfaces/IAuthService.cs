0a73b2df39b29cd15067d49ae098e62698a2be24
saifsayyedsnapfluence <154328634+saifsayyedsnapfluence@users.noreply.github.com>
2023-12-21 15:19:42 -0800

    first
    allah madat kare

---
+++ JWT_Authentication_Authorization/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using JWT_Authentication_Authorization.Models;
+using JWT_Authentication_Authorization.Models.Response;
+using Microsoft.AspNetCore.Identity.Data;
+namespace JWT_Authentication_Authorization.Interfaces
+{
+    public interface IAuthService
+    {
+        User AddUser(User user);
+        LoginResponse Login(Models.LoginRequest loginRequest);
+        Role AddRole(Role role);
+        bool AssignRoleToUser(AddUserRole obj);
+    }
+}
---
+++ JWT_Authentication_Authorization/Models/Response/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace JWT_Authentication_Authorization.Models.Response
+{
+    public class LoginResponse
+    {
+        public User User { get; set; }
+        public string Token { get; set; }
+        public bool isSucesss { get; set; }
+        public string Error { get; set; }
+        public List<UserRole> Roles { get; set; }
+        public List<Role> RoleName { get; set; }
+
+
+    }
+}
